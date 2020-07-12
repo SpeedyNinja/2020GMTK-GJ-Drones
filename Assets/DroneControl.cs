71ad9326bb1aa3ee5eeadac0292675191a1882ac
Daniel <dwm.kiwi@gmail.com>
2020-07-12 17:04:54 +1200

    death animations

--- Assets/DroneControl.cs
+++ Assets/DroneControl.cs
@@ -8,6 +8,8 @@ using Random = UnityEngine.Random;
 
 public class DroneControl : MonoBehaviour
 {
+    public float deathDelay;
+    
     private Transform _transform;
     private SpriteRenderer _spriteRenderer;
 
@@ -15,13 +17,13 @@ public class DroneControl : MonoBehaviour
     private float _scale;
 
     private bool _dying;
-    private float _deathDelay;
     private float _deathCountdown;
     private float _health;
     private float _max_health;
     private float _speed;
     private float _zigOffset;
     private float _zigAmount;
+    private float _dead_direction;
     private Vector3 _startPos;
     private static readonly int GlowVal = Shader.PropertyToID("_GlowVal");
 
@@ -34,6 +36,7 @@ public class DroneControl : MonoBehaviour
         _spriteRenderer.color = _colour;
         _startPos = _transform.position;
         _max_health = _health;
+        _deathCountdown = deathDelay;
     }
 
     public void SetVars(float newHealth, float scale, Color newColour, float newSpeed, float zagOffset, float zagAmount)
@@ -48,28 +51,46 @@ public class DroneControl : MonoBehaviour
 
     private void Update()
     {
-        var newY = _transform.position.y - _speed * Time.deltaTime;
-        var xDelta = Convert.ToSingle(_zigAmount * Math.Sin(newY + _zigOffset));
-        var newX = xDelta + _startPos.x;
-        var pos = new Vector3(newX, newY);
-        var angle = Quaternion.Euler(0, 0, Convert.ToSingle(-180 - (_zigAmount * Math.Cos(newY + _zigOffset) * 45)));
-        _transform.SetPositionAndRotation(pos, angle);
-        _spriteRenderer.material.SetFloat(GlowVal, _health / _max_health + 1);
+        if (!_dying)
+        {
+            var newY = _transform.position.y - _speed * Time.deltaTime;
+            var xDelta = Convert.ToSingle(_zigAmount * Math.Sin(newY + _zigOffset));
+            var newX = xDelta + _startPos.x;
+            var pos = new Vector3(newX, newY);
+            var angle = Quaternion.Euler(0, 0, Convert.ToSingle(-180 - (_zigAmount * Math.Cos(newY + _zigOffset) * 45)));
+            _transform.SetPositionAndRotation(pos, angle);
+            _spriteRenderer.material.SetFloat(GlowVal, _health / _max_health + 1);
+        }
+        else
+        {
+            if (_deathCountdown > 0)
+            {
+                _deathCountdown -= Time.deltaTime;
+                _spriteRenderer.color = new Color(_colour.r, _colour.g, _colour.b, _deathCountdown / deathDelay);
+                var newY = _transform.position.y - _speed * _deathCountdown / deathDelay * Time.deltaTime;
+                var pos = new Vector3(_transform.position.x, newY);
+                var angle = Quaternion.Euler(0, 0, _transform.rotation.eulerAngles.z + (Time.deltaTime * 360 * _deathCountdown / deathDelay));
+                _transform.SetPositionAndRotation(pos, angle);
+            }
+            else
+            {
+                Object.Destroy(gameObject);
+            }
+        }
     }
 
     private void FixedUpdate()
     {
         if (_health <= 0)
         {
-            // TODO: fancy blow up
-            Object.Destroy(gameObject);
+            gameObject.GetComponent<PolygonCollider2D>().enabled = false;
+            _dying = true;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         _health -= 1;
-        Debug.Log("hit a thing" + _health);
     }
 
     private void OnTriggerExit2D(Collider2D other)
